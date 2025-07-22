fe90a7b09aa3ee6989a0153d99b657ed6df182b5
ajpelu <ajpelu@gmail.com>
2025-07-22 20:00:13 +0200

    add some utils

---
+++ R/utils.R
@@ -0,0 +1,21 @@
+#' Classify species based on thresholds
+#' This function classifies species into categories based on their variable type and thresholds.
+#' @param df A data frame containing the species data.
+#' @param var_type The variable type to classify (e.g., "type_A1", "type_A2").
+#' @param thresholds A numeric vector containing the thresholds for classification.
+#' @param var_th The variable to use for classification (e.g., "A_Pop_red").
+#' @return A character vector with the classification results.
+
+
+.classify <- function(df, var_type, thresholds, var_th) {
+  v <- df[[var_th]]
+  th <- thresholds
+
+  dplyr::case_when(
+    df[[var_type]] == 1 & v >= th[1] ~ "CR",
+    df[[var_type]] == 1 & v >= th[2] & v < th[1] ~ "EN",
+    df[[var_type]] == 1 & v >= th[3] & v < th[2] ~ "VU",
+    df[[var_type]] == 1 & v < th[3] ~ "NT",
+    TRUE ~ NA_character_
+  )
+}
